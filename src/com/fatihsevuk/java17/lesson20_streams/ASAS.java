42c7689bef3f014ea6cc6b6341ce52ed20dd1bbb
fatihsevuk <fatihsvk1@gmail.com>
2025-10-24 11:48:18 +0200

    last changes

---
+++ src/com/fatihsevuk/java17/lesson20_streams/ASAS.java
@@ -0,0 +1,11 @@
+package com.fatihsevuk.java17.lesson20_streams;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+    }
+}
--- src/com/fatihsevuk/java17/lesson20_streams/ReduceExample.java
+++ src/com/fatihsevuk/java17/lesson20_streams/ReduceExample.java
@@ -1,4 +1,28 @@
 package com.fatihsevuk.java17.lesson20_streams;
 
+import java.util.List;
+
 public class ReduceExample {
+    public static void main(String[] args) {
+
+/*        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+
+        int sum = numbers.stream()
+                .reduce(1, (a, b) -> a + b);
+
+        System.out.println(sum);  // Output: 15*/
+
+        List<String> words = List.of("Java", "Stream", "Reduce");
+
+        int totalLength = words.stream()
+                .reduce(0,
+                        (sum, word) -> sum + word.length(),  // accumulator
+                        Integer::sum);                      // combiner
+
+        System.out.println(totalLength);  // Output: 14
+
+
+    }
 }
+
+

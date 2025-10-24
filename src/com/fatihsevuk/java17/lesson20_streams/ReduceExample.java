c26f46917ee665d53be72b660418b9a2df760ae7
fatihsevuk <fatihsvk1@gmail.com>
2025-10-24 12:04:10 +0200

    last changes- 3434343

--- src/com/fatihsevuk/java17/lesson20_streams/ReduceExample.java
+++ src/com/fatihsevuk/java17/lesson20_streams/ReduceExample.java
@@ -3,5 +3,8 @@ package com.fatihsevuk.java17.lesson20_streams;
 public class ReduceExample {
     public static void main(String[] args) {
         System.out.println("changes 11111");
+        System.out.println("changes 222");
+        System.out.println("changes 2222");
+        System.out.println("changes 22222");
     }
 }
